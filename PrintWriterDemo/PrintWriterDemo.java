238ecd50a1ab88a66f2c5ba6e0d39a2bf3e0be6b
work <work@localhost.localdomain>
2017-03-02 01:25:12 -0800

    添加字符流练习程序

---
+++ PrintWriterDemo/PrintWriterDemo.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class PrintWriterDemo{
+	public static void main(String[] args) {
+		PrintWriter pw = new PrintWriter(System.out,true );
+		int i = 10;
+		double d = 123.56;
+		
+		pw.println("Using a PrintWriter.");
+		pw.println(i);
+		pw.println(d);
+
+		pw.println(i+"+"+d+" is "+(i+d));
+		
+	}
+
+}
